6fe46e7ed6f45c267732f81955115cf0948e784c
Daniel Portugal <daniel.epr77@gmail.com>
2023-02-19 18:41:09 -0500

    se agrega codigo python

---
+++ PYTHON/python_101.py
@@ -0,0 +1,18 @@
+
+
+lista = ['papaya','torojna','uva','platano','maracuya','mango']
+
+
+def agregar_varlo_lista():
+    lista.append('Frutilla')
+
+
+def run():
+    agregar_varlo_lista()
+    print(lista)
+
+
+if __name__ == '__main__':
+    run()
+
+
---
+++ PYTHON/readme.md
@@ -0,0 +1,11 @@
+```
+python -m venv .env
+(win) source .env/Scripts/activate
+(Linux) source .env/bin/activate
+
+deactivate
+```
+
+```
+pip install pandas
+```
